3f3fc9c143666a0422134c34611f72c4ebfc57cb
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2013-01-16 19:03:06 +0100

    Added inverse

--- include/OGDT/math/mat3.h
+++ include/OGDT/math/mat3.h
@@ -15,15 +15,15 @@ A column-major 3x3 matrix.
 class DECLDIR mat3
 {
     float val[3][3];
-    
+
 public:
-    
+
     /*
     Constructor: mat3
     Construct a matrix and set it to the identity.
     */
     mat3 ();
-    
+
     /*
     Constructor: mat3
     Construct a matrix from the given values.
@@ -31,29 +31,29 @@ public:
     mat3 (float m00, float m10, float m20
          ,float m01, float m11, float m21
          ,float m02, float m12, float m22);
-    
+
     /*
     Constructor: mat3
     Construct a matrix from the given vectors.
-    
+
     Each of the vectors represents a column of the matrix.
     */
     mat3 (const vec3& v0
          ,const vec3& v1
          ,const vec3& v2);
-    
+
     /*
     Constructor: mat3
     Construct a transformation matrix from the given vectors.
     */
     mat3 (const vec2& right, const vec2& up, const vec2& position);
-        
+
     /*
     Operator: ()
     Return a mutable reference to the value at the specified position.
     */
     float& operator () (int row, int col);
-    
+
     /*
     Operator: ()
     Access the value at the specified position.
@@ -65,31 +65,31 @@ public:
     Return a mutable reference to the matrix's first column.
     */
     vec2& v0 ();
-    
+
     /*
     Function: v1
     Return a mutable reference to the matrix's second column.
     */
     vec2& v1 ();
-    
+
     /*
     Function: v2
     Return a mutable reference to the matrix's third column.
     */
     vec2& v2 ();
-    
+
     /*
     Function: v0
     Return the matrix's first column.
     */
     const vec2& v0 () const;
-    
+
     /*
     Function: v1
     Return the matrix's second column.
     */
     const vec2& v1 () const;
-    
+
     /*
     Function: v2
     Return the matrix's third column.
@@ -101,7 +101,7 @@ public:
     Multiply two matrices.
     */
     mat3 operator* (const mat3&);
-    
+
     /*
     Operator: *=
     Multiply two matrices and accumulate the result in the first operand.
@@ -125,7 +125,7 @@ public:
     Return the rotation component of the matrix.
     */
     mat3 rot () const;
-    
+
     /*
     Function: rot
     Create a rotation matrix.
@@ -135,17 +135,17 @@ public:
     angle - The angle of rotation in degrees.
     */
     static mat3 rot (float angle);
-    
+
     /*
     Function: scale
     Create a scale matrix.
-    
+
     Parameters:
 
     s - A vector specifying the scale factor on each axis.
     */
     static mat3 scale (vec3 s);
-    
+
     /*
     Function: scale
     Create a scale matrix.
@@ -157,7 +157,7 @@ public:
     sz - The scale factor on the Z axis.
     */
     static mat3 scale (float x, float y, float z);
-    
+
     /*
     Function: transl
     Create a translation matrix.
@@ -167,7 +167,7 @@ public:
     offset - A vector specifying the translation along each axis.
     */
     static mat3 transl (vec2 offset);
-    
+
     /*
     Function: transl
     Create a translation matrix.
@@ -178,8 +178,36 @@ public:
     y - The amount of translation along the Y axis.
     */
     static mat3 transl (float x, float y);
+
+    /*
+    Constant: reflectx
+    The X-axis reflection matrix.
+    */
+    static const mat3 reflectx;
+
+    /*
+    Constant: reflecty
+    The Y-axis reflection matrix.
+    */
+    static const mat3 reflecty;
+
+    /*
+    Constant: reflectz
+    The Z-axis reflection matrix.
+    */
+    static const mat3 reflectz;
+
+    /*
+    Constant: id
+    The identity matrix.
+    */
+    static const mat3 id;
 };
 
+DECLDIR mat3 inverse (const mat3&);
+
 DECLDIR mat3 transpose (const mat3&);
+
+vec3 operator* (const mat3&, vec3);
 
 } // namespace OGDT
--- src/math/mat3.cc
+++ src/math/mat3.cc
@@ -45,7 +45,7 @@ mat3 mat3::rot (float angle)
     float a  = angle * (float) M_PI / 180.0f;
     float sa = sin (a);
     float ca = cos (a);
-    
+
     return mat3
         (ca, -sa, 0
         ,sa,  ca, 0
@@ -144,15 +144,15 @@ mat3 mat3::operator* (const mat3& m)
     float m00 = a(0,0) * m(0,0) + a(0,1) * m(1,0) + a(0,2) * m(2,0);
     float m01 = a(0,0) * m(0,1) + a(0,1) * m(1,1) + a(0,2) * m(2,1);
     float m02 = a(0,0) * m(0,2) + a(0,1) * m(1,2) + a(0,2) * m(2,2);
-    
+
     float m10 = a(1,0) * m(0,0) + a(1,1) * m(1,0) + a(1,2) * m(2,0);
     float m11 = a(1,0) * m(0,1) + a(1,1) * m(1,1) + a(1,2) * m(2,1);
     float m12 = a(1,0) * m(0,2) + a(1,1) * m(1,2) + a(1,2) * m(2,2);
-    
+
     float m20 = a(2,0) * m(0,0) + a(2,1) * m(1,0) + a(2,2) * m(2,0);
     float m21 = a(2,0) * m(0,1) + a(2,1) * m(1,1) + a(2,2) * m(2,1);
     float m22 = a(2,0) * m(0,2) + a(2,1) * m(1,2) + a(2,2) * m(2,2);
-    
+
     return mat3
         (m00, m01, m02
         ,m10, m11, m12
@@ -167,15 +167,15 @@ void mat3::operator*= (const mat3& m)
     float m00 = a(0,0) * m(0,0) + a(0,1) * m(1,0) + a(0,2) * m(2,0);
     float m01 = a(0,0) * m(0,1) + a(0,1) * m(1,1) + a(0,2) * m(2,1);
     float m02 = a(0,0) * m(0,2) + a(0,1) * m(1,2) + a(0,2) * m(2,2);
-    
+
     float m10 = a(1,0) * m(0,0) + a(1,1) * m(1,0) + a(1,2) * m(2,0);
     float m11 = a(1,0) * m(0,1) + a(1,1) * m(1,1) + a(1,2) * m(2,1);
     float m12 = a(1,0) * m(0,2) + a(1,1) * m(1,2) + a(1,2) * m(2,2);
-    
+
     float m20 = a(2,0) * m(0,0) + a(2,1) * m(1,0) + a(2,2) * m(2,0);
     float m21 = a(2,0) * m(0,1) + a(2,1) * m(1,1) + a(2,2) * m(2,1);
     float m22 = a(2,0) * m(0,2) + a(2,1) * m(1,2) + a(2,2) * m(2,2);
-    
+
     *this = mat3
         (m00, m01, m02
         ,m10, m11, m12
@@ -203,10 +203,80 @@ mat3 mat3::rot () const
 }
 
 
+const mat3 mat3::reflectx = mat3
+    (-1, 0, 0
+    , 0, 1, 0
+    , 0, 0, 1);
+
+
+const mat3 mat3::reflecty = mat3
+    (1,  0,  0
+    ,0, -1,  0
+    ,0,  0,  1);
+
+
+const mat3 mat3::reflectz = mat3
+    (1,  0,  0
+    ,0,  1,  0
+    ,0,  0, -1);
+
+
+const mat3 mat3::id = mat3
+    ( 1.0f, 0.0f, 0.0f
+    , 0.0f, 1.0f, 0.0f
+    , 0.0f, 0.0f, 1.0f);
+
+
+mat3 OGDT::inverse (const mat3& m)
+{
+    float m00 = m[0];
+    float m01 = m[1];
+    float m02 = m[2];
+    float m03 = m[3];
+    float m04 = m[4];
+    float m05 = m[5];
+    float m06 = m[6];
+    float m07 = m[7];
+    float m08 = m[8];
+
+    float det = m00*(m04*m08-m05*m07) - m03*(m01*m08-m02*m07) + m06*(m01*m05-m02*m04);
+    if (det == 0.0f)
+    {
+        return mat3::id;
+    }
+
+    float d10 = m04*m08 - m07*m05;
+    float d11 = m03*m08 - m06*m05;
+    float d12 = m03*m07 - m06*m04;
+    float d20 = m01*m08 - m07*m02;
+    float d21 = m00*m08 - m06*m02;
+    float d22 = m00*m07 - m06*m01;
+    float d30 = m01*m05 - m04*m02;
+    float d31 = m00*m05 - m03*m02;
+    float d32 = m00*m04 - m03*m01;
+
+    det = det / 1.0f;
+
+    return mat3
+            ( d10*det, -d11*det,  d12*det
+            ,-d20*det,  d21*det, -d22*det
+            , d30*det, -d31*det,  d32*det);
+}
+
+
 mat3 OGDT::transpose (const mat3& m)
 {
     return mat3
         (m(0,0), m(1,0), m(2,0)
         ,m(0,1), m(1,1), m(2,1)
         ,m(0,2), m(1,2), m(2,2));
+}
+
+
+vec3 OGDT::operator* (const mat3& m, vec3 v)
+{
+    return vec3
+        (m[0]*v.x + m[1]*v.y + m[2]*v.z,
+         m[3]*v.x + m[4]*v.y + m[5]*v.z,
+         m[6]*v.x + m[7]*v.y + m[8]*v.z);
 }
