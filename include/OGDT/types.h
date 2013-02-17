8ff82a91a2bed4e840f1b5ee19ef29409d4fdb17
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2013-02-17 20:17:36 +0100

    Fixed whitespace

--- include/OGDT/types.h
+++ include/OGDT/types.h
@@ -7,14 +7,14 @@ File: Types
 */
 
 #if defined(__GNUC__) || defined(__GNUG__) //Compiling with GNU C/C++ compiler
-    typedef __INT8_TYPE__    I8;
-    typedef __INT16_TYPE__    I16;
-    typedef __INT32_TYPE__    I32;
-    typedef __INT64_TYPE__    I64;
-    typedef __UINT8_TYPE__    U8;
-    typedef __UINT16_TYPE__    U16;
-    typedef __UINT32_TYPE__    U32;
-    typedef __UINT64_TYPE__    U64;
+    typedef __INT8_TYPE__   I8;
+    typedef __INT16_TYPE__  I16;
+    typedef __INT32_TYPE__  I32;
+    typedef __INT64_TYPE__  I64;
+    typedef __UINT8_TYPE__  U8;
+    typedef __UINT16_TYPE__ U16;
+    typedef __UINT32_TYPE__ U32;
+    typedef __UINT64_TYPE__ U64;
 #else //Visual Studio
     /*
     Typedef: I8
@@ -40,7 +40,7 @@ File: Types
     Typedef: I8
     An unsigned 8-bit integer.
     */
-    typedef unsigned __int8        U8;
+    typedef unsigned __int8     U8;
     /*
     Typedef: I16
     An unsigned 16-bit integer.
