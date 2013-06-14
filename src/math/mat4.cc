584274e77e06efbcd4455a5aa8018476306800b4
jeannekamikaze <jeannekamikaze@gmail.com>
2013-06-14 16:25:13 +0300

    Fixed transform

--- src/math/mat4.cc
+++ src/math/mat4.cc
@@ -576,8 +576,8 @@ mat4 OGDT::transpose (const mat4& m)
 vec3 OGDT::transform (const mat4& m, vec3 v, float w)
 {
     vec3 u;
-    u.x = m(0,0) * v.x + m(0,1) * v.y + m(0,2) * v.z + w;
-    u.y = m(1,0) * v.x + m(1,1) * v.y + m(1,2) * v.z + w;
-    u.z = m(2,0) * v.x + m(2,1) * v.y + m(2,2) * v.z + w;
+    u.x = m(0,0) * v.x + m(0,1) * v.y + m(0,2) * v.z + m(0,3) * w;
+    u.y = m(1,0) * v.x + m(1,1) * v.y + m(1,2) * v.z + m(1,3) * w;
+    u.z = m(2,0) * v.x + m(2,1) * v.y + m(2,2) * v.z + m(2,3) * w;
     return u;
 }
